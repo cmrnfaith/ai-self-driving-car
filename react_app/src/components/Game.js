aa68dc206d456e4fc356c04f683120251aa77315
cydro <cmrnfaith@gmail.com>
2023-01-19 19:49:26 -0700

    updates to car

--- react_app/src/components/Game.js
+++ react_app/src/components/Game.js
@@ -5,6 +5,10 @@ import Car from "./Car";
 
 const ball_radius = 5;
 const car_size = { height: 20, width: 10 };
+const car_pos = (center) => ({ x: center.x - 10, y: center.y - 5 });
+const ball1_pos = (center) => ({ x: center.x - 17.5, y: center.y - 7.5 });
+const ball2_pos = (center) => ({ x: center.x - 5, y: center.y - 12.5 });
+const ball3_pos = (center) => ({ x: center.x + 7.5, y: center.y - 7.5 });
 
 function Game({ angle, center }) {
   const carRef = useRef();
@@ -21,24 +25,24 @@ function Game({ angle, center }) {
         <Car
           ref={carRef}
           angle={angle}
-          pos={{ x: center.x - 25, y: center.y - 5 }}
+          pos={car_pos(center)}
           size={car_size}
         />
         <Ball
           ref={ball1Ref}
-          pos={{ x: center.x - 17.5, y: center.y - 7.5 }}
+          pos={ball1_pos(center)}
           radius={ball_radius}
           fill={ballOneFill}
         />
         <Ball
           ref={ball2Ref}
-          pos={{ x: center.x - 5, y: center.y - 12.5 }}
+          pos={ball2_pos(center)}
           radius={ball_radius}
           fill={ballTwoFill}
         />
         <Ball
           ref={ball3Ref}
-          pos={{ x: center.x + 7.5, y: center.y - 7.5 }}
+          pos={ball3_pos(center)}
           radius={ball_radius}
           fill={ballThreeFill}
         />
