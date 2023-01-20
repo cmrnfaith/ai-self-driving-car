b8a6bd0656f0465d38b4aa6a07cf4e0a6b3520e5
cydro <cmrnfaith@gmail.com>
2023-01-19 22:11:09 -0700

    Prevent scroll on mobile

--- react_app/src/components/PaintWidget.js
+++ react_app/src/components/PaintWidget.js
@@ -1,60 +1,74 @@
 import { useState, useRef } from "react";
-import { Layer, Line, Stage, Text } from "react-konva";
+import { Layer, Line, Stage } from "react-konva";
 
 function PaintWidget({ barriers, setBarriers, width, height }) {
-    const [tool, setTool] = useState('pen');
-    
-    const isDrawing = useRef(false);
-  
-    const handleMouseDown = (e) => {
-      isDrawing.current = true;
-      const pos = e.target.getStage().getPointerPosition();
-      setBarriers([...barriers, { tool, points: [pos.x, pos.y] }]);
-    };
-  
-    const handleMouseMove = (e) => {
-      // no drawing - skipping
-      if (!isDrawing.current) {
-        return;
-      }
-      const stage = e.target.getStage();
-      const point = stage.getPointerPosition();
-      let lastLine = barriers[barriers.length - 1];
-      // add point
-      lastLine.points = lastLine.points.concat([point.x, point.y]);
-  
-      // replace last
-      barriers.splice(barriers.length - 1, 1, lastLine);
-      setBarriers(barriers.concat());
-    };
-  
-    const handleMouseUp = () => {
-      isDrawing.current = false;
-    };
+  const [tool, setTool] = useState("pen");
 
-    const ClearCanvas = () => {
-        setBarriers([]);
+  const isDrawing = useRef(false);
+
+  const handleMouseDown = (e) => {
+    isDrawing.current = true;
+    const pos = e.target.getStage().getPointerPosition();
+    setBarriers([...barriers, { tool, points: [pos.x, pos.y] }]);
+  };
+
+  const handleMouseMove = (e) => {
+    // no drawing - skipping
+    if (!isDrawing.current) {
+      return;
     }
-  
-    return (
-      <div>
-       <select
-          value={tool}
-          onChange={(e) => {
-            setTool(e.target.value);
-          }}
-        >
-          <option value="pen">Pen</option>
-          <option value="eraser">Eraser</option>
-        </select>
-        <button onClick={ClearCanvas}>Clear Canvas</button>
-        <div className="stage-wrapper">
+    const stage = e.target.getStage();
+    const point = stage.getPointerPosition();
+    let lastLine = barriers[barriers.length - 1];
+    // add point
+    lastLine.points = lastLine.points.concat([point.x, point.y]);
+
+    // replace last
+    barriers.splice(barriers.length - 1, 1, lastLine);
+    setBarriers(barriers.concat());
+  };
+
+  const handleMouseUp = () => {
+    isDrawing.current = false;
+  };
+
+  const handleTouchStart = (event) => {
+    event.evt.preventDefault();
+    console.log(event)
+    handleMouseDown(event);
+  }
+
+  const handleTouchMove = (event) => {
+    event.evt.preventDefault();
+    handleMouseMove(event);
+  }
+
+  const ClearCanvas = () => {
+    setBarriers([]);
+  };
+
+  return (
+    <div>
+      <select
+        value={tool}
+        onChange={(e) => {
+          setTool(e.target.value);
+        }}
+      >
+        <option value="pen">Pen</option>
+        <option value="eraser">Eraser</option>
+      </select>
+      <button onClick={ClearCanvas}>Clear Canvas</button>
+      <div className="stage-wrapper">
         <Stage
           width={width}
           height={height}
           onMouseDown={handleMouseDown}
-          onMousemove={handleMouseMove}
-          onMouseup={handleMouseUp}
+          onMouseMove={handleMouseMove}
+          onMouseUp={handleMouseUp}
+          onTouchStart={handleTouchStart}
+          onTouchMove={handleTouchMove}
+          onTouchEnd={handleMouseUp}
           className="stage"
         >
           <Layer>
@@ -68,15 +82,15 @@ function PaintWidget({ barriers, setBarriers, width, height }) {
                 lineCap="round"
                 lineJoin="round"
                 globalCompositeOperation={
-                    barriers.tool === 'eraser' ? 'destination-out' : 'source-over'
+                  barriers.tool === "eraser" ? "destination-out" : "source-over"
                 }
               />
             ))}
           </Layer>
         </Stage>
-        </div> 
       </div>
-      );
+    </div>
+  );
 }
 
 export default PaintWidget;
